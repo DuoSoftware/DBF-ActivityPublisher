c9118887e752f56eee5bb16b4a77728fa089bd26
binaraGoonawardana <binara.is@live.com>
2018-09-23 11:02:28 +0530

    npm install while publishing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,21 +2,10 @@
 
 sudo su
 
-unzip smoothflow-activity-template.zip
+unzip $(find ./ -iname \*.zip)
 
-cd smoothflow-activity-template
+cd $(ls -d */)
 
 npm install
-
-# npm config set scope binaa
-
-# TOKEN=$(curl -s \
-#   -H "Accept: application/json" \
-#   -H "Content-Type:application/json" \
-#   -X PUT --data '{"name": "binaa", "password": "tc1dabest"}' \
-#   http://registry.npmjs.org/-/user/org.couchdb.user:binaa 2>&1 | grep -Po \
-#   '(?<="token": ")[^"]*')
-
-# npm set registry "https://registry.npmjs.org/:_authToken c8f34063-d569-46ee-a13c-87f3a39c4655"
 
 npm publish
